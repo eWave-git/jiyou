1087f22576e46132cc5493ac723f7c0eb3ffc29f
simtaejin <stj53@nate.com>
2023-08-04 13:37:13 +0900

    코딩 완료

--- routes/manager/inquiry.php
+++ routes/manager/inquiry.php
@@ -11,3 +11,21 @@ $obRouter->get('/manager/inquiry',[
         return new Response(200, Manager\Inquiry::getInquiry($request));
     }
 ]);
+
+$obRouter->get('/manager/chart_inquiry',[
+    'middlewares' => [
+        'required-manager-login'
+    ],
+    function($request) {
+        return new Response(200, Manager\Inquiry::getChartInquiry($request));
+    }
+]);
+
+$obRouter->get('/manager/table_inquiry',[
+    'middlewares' => [
+        'required-manager-login'
+    ],
+    function($request) {
+        return new Response(200, Manager\Inquiry::getTableInquiry($request));
+    }
+]);
