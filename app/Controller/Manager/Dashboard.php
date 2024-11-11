f2bc804ce326c371ebbe645af639296ee6e0fdeb
simtaejin <stj53@nate.com>
2024-11-11 15:05:10 +0900

    불필요한 주석 삭제 및 모바일 화면 깨지는 현상 수정

--- app/Controller/Manager/Dashboard.php
+++ app/Controller/Manager/Dashboard.php
@@ -16,159 +16,6 @@ use App\Utils\View;
 class Dashboard extends Page {
     const DEFAULT_CHECK_TIME = 5;
 
-//    public static function getWidgetBoard($request) {
-//        $postVars = $request->getPostVars();
-//        $obj = EntityDevice::getDevicesByIdx($postVars['device_idx']);
-//
-//        $results = Management::getBoardTypeName($obj->board_type);
-//
-//        $arr = array();
-//
-//        if ($results) {
-//            foreach ($results as $k => $v) {
-//                $arr['idx'][] =  $v['field'];
-//                $arr['text'][] =  $v['name'];
-//            }
-//        } else {
-//            $arr['idx'][] =  '';
-//            $arr['text'][] =  '';
-//        }
-//
-//        return [
-//            'success' => true,
-//            'idx'=>$arr['idx'],
-//            'text' => $arr['text'],
-//        ];
-//    }
-
-//    private static function getIntervalOption() {
-//        $option = "";
-//
-//        $interval = Common::getInterval();
-//
-//        foreach ($interval as $k => $v) {
-//            $option .= View::render('manager/modules/dashboard/widget_add_form_options', [
-//                'value' => $k,
-//                'text'  => $v,
-//                'selected' => '',
-//            ]);
-//        }
-//
-//        return $option;
-//    }
-
-//    private static function getMemberDevice($user_idx) {
-//        $option = "";
-//
-//        $member_devices = Common::getMembersDevice($user_idx);
-//
-//        if ($member_devices[0]['idx']) {
-//            if (is_array($member_devices[0])) {
-//                foreach ($member_devices as $k => $v) {
-//                    $option .= View::render('manager/modules/dashboard/widget_add_form_options', [
-//                        'value' => $v['idx'],
-//                        'text'  => $v['address']."-".$v['board_type']."-".$v['board_number'],
-//                        'selected' => '',
-//                    ]);
-//                }
-//            }
-//        }
-//
-//        return $option;
-//    }
-
-//    private static function widget_add($user_idx) {
-//        $item = "";
-//        $item = View::render('manager/modules/dashboard/widget_add_form', [
-//            'device_options' => self::getMemberDevice($user_idx),
-//            'interval_options' => self::getIntervalOption(),
-//        ]);
-//
-//        return $item;
-//    }
-
-//    private static function wigget_table($user_idx) {
-//
-//        $item = "";
-//
-//        $result_1 = EntityWidget::getWidgeTablebyMemberIdx($user_idx);
-//
-//        $array = array();
-//        $i = 0;
-//        while ($obj_1 = $result_1->fetchObject(EntityWidget::class)) {
-//            $array[$i]['info'] = (array)$obj_1;
-//
-//            $result_2 = EntityWidgetData::getWidgetDatesByWidgetIdx($obj_1->idx);
-//            $j = 0;
-//
-//            while ($obj_2 = $result_2->fetchObject(EntityWidgetData::class)) {
-//                $array[$i]['datas'][$j] = (array)$obj_2;
-//                $symbol = Common::findSymbol($obj_2->board_type_name);
-//
-//                if (isset($symbol['idx'])) {
-//                    $array[$i]['datas'][$j]['symbol'] = $symbol['symbol'];
-//                    $array[$i]['datas'][$j]['standard'] = $symbol['standard'];
-//                }
-//                $j++;
-//            }
-//            $i++;
-//        }
-//
-//        $table_arr = array();
-//
-//        foreach ($array as $k_1 => $v_1) {
-//            $result_3 = EntityRawData::LastLimitDataOne($v_1['datas'][0]['address'], $v_1['datas'][0]['board_type'], $v_1['datas'][0]['board_number'], $v_1['datas'][0]['board_type_field'], $v_1['datas'][0]['board_type_name']);
-//            $obj_3 = $result_3->fetchObject(EntityRawData::class);
-//            $table_arr[$k_1]['title'] = $v_1['info']['widget_name'];
-//            $table_arr[$k_1]['idx'] = $v_1['info']['idx'];
-//            $table_arr[$k_1]['text'] = $v_1['datas'][0]['board_type_name'];
-//            $table_arr[$k_1]['value'] = $obj_3->{$v_1['datas'][0]['board_type_name']} ?? 0;
-//            $table_arr[$k_1]['symbol'] = $v_1['datas'][0]['symbol'] ?? '';
-//            $table_arr[$k_1]['standard'] = $v_1['datas'][0]['standard'] ?? '';
-//        }
-//
-//        foreach ($table_arr as $k => $v) {
-//
-//            if ($v['standard']) {
-//                if ($v['symbol'] == "PPM") {
-//                    $v['value'] =  ($v['value']*100) / 5000 ;
-//                } else if ($v['symbol'] == "ug/㎥") {
-//                    $v['value'] =  ($v['value']*100) / 1000 ;
-//                } else if ($v['symbol'] == "lux") {
-//                    $v['value'] =  ($v['value']*100) / 65000 ;
-//                }
-//            }
-//
-//            $item .= View::render('manager/modules/dashboard/widget_table', [
-//                'title' => $v['title'],
-//                'idx' => $v['idx'],
-//                'text' => $v['text'],
-//                'symbol' => $v['symbol'],
-//                'value' => round($v['value'],1),
-//                'update_date' => date("m-d H:i"),
-//            ]);
-//        }
-//
-//        return $item;
-//    }
-
-//    private static function widget_chart($user_idx) {
-//
-//        $item = "";
-//
-//        $result = EntityWidget::getWidgeChartbyMemberIdx($user_idx);
-//
-//        while($obj = $result->fetchObject(EntityWidget::class)) {
-//            $item .= View::render('manager/modules/dashboard/widget_chart', [
-//                'title' => $obj->widget_name,
-//                'idx' => $obj->idx,
-//                'chart_idx' => "myChart_".$obj->idx,
-//            ]);
-//        }
-//
-//        return $item;
-//    }
-
     public static function getCardItem($rew_obj, $board_name) {
         $item = "";
 
@@ -229,7 +76,7 @@ class Dashboard extends Page {
     }
 
 
-    public static function getWidgetCard($user_idx) {
+    public static function getWidgetCard($user_idx, $display) {
         $obj = Common::getMembersWidget($user_idx);
 
         $card = "";
@@ -257,6 +104,7 @@ class Dashboard extends Page {
                     'subject' => $obj[$k]['widget_name'],
                     'idx' => $obj[$k]['idx'],
                     'item' => self::getCardItem($rew_obj, $v['board_name']),
+                    'display' => $display,
                     'check_class' => $check_class,
                     'check_text' => $check_text,
                     'update_at' => isset($rew_obj->created_at) ? substr($rew_obj->created_at, 5, 14) : "00-00 00:00:00" ,
@@ -297,7 +145,7 @@ class Dashboard extends Page {
 
         $content = View::render('manager/modules/dashboard/index', [
             'farm_name' => $_farm_Info->farm_name ?? '',
-            'widget_card' => self::getWidgetCard($_userInfo->idx),
+            'widget_card' => self::getWidgetCard($_userInfo->idx, $display),
             'display' => $display,
         ]);
 
@@ -496,143 +344,6 @@ class Dashboard extends Page {
         ];
         return $array;
     }
-
-
-//    public static function setWidgetAdd($request) {
-//
-//        $_user = Common::get_manager();
-//        $_userInfo = EntityMmeber::getMemberById($_user);
-//
-//        $postVars = $request->getPostVars();
-//
-//        $device_info = EntityDevice::getDevicesByIdx($postVars['widget_device']);
-//
-//        $board_type_info = Management::getBoardTypeName($device_info->board_type);
-//
-//        $obj = new EntityWidget;
-//        $obj->member_idx = $_userInfo->idx;
-//        $obj->widget_name = $postVars['widget_name'];
-//        $obj->widget_type = $postVars['widget_type'];
-//        $obj->graph_interval = $postVars['widget_graph_interval'];
-//
-//        $_idx = $obj->created();
-//
-//        $obj_data = new EntityWidgetData();
-//        $obj_data->widget_idx = $_idx;
-//        $obj_data->device_idx = $device_info->idx;
-//        $obj_data->address = $device_info->address;
-//        $obj_data->board_type = $device_info->board_type;
-//        $obj_data->board_number = $device_info->board_number;
-//
-//        foreach($board_type_info as $k => $v) {
-//            if ($postVars['widget_board'] == $v['field']) {
-//                $obj_data->board_type_field = $board_type_info[$k]['field'];
-//                $obj_data->board_type_name = $board_type_info[$k]['name'];
-//            }
-//        }
-//
-//        $obj_data->created();
-//
-//        $request->getRouter()->redirect('/manager/dashboard');
-//    }
-//    public static function setWidgetRemove($request, $idx) {
-//
-//        $obj = EntityWidget::getWidgets("idx='".$idx."'",'','','*')->fetchObject(EntityWidget::class);
-//        $obj->deleted();
-//
-//        $obj = EntityWidgetData::getWidgetDatesByWidgetIdx($idx)->fetchObject(EntityWidgetData::class);
-//        $obj->deleted();
-//
-//        $request->getRouter()->redirect('/manager/dashboard');
-//    }
-
-
-
-//    public static function getMyChart($request) {
-//        $postVars = $request->getPostVars();
-//
-//        $_user = Common::get_manager();
-//        $_userInfo = EntityMmeber::getMemberById($_user);
-//
-//        $result_1 = EntityWidget::getWidgeChartbyMemberIdx($_userInfo->idx);
-//
-//        $array = array();
-//        $i = 0;
-//        while ($obj_1 = $result_1->fetchObject(EntityWidget::class)) {
-//            $array[$i]['info'] = (array)$obj_1;
-//
-//            $result_2 = EntityWidgetData::getWidgetDatesByWidgetIdx($obj_1->idx);
-//            $j = 0;
-//
-//            while ($obj_2 = $result_2->fetchObject(EntityWidgetData::class)) {
-//                $array[$i]['datas'][$j] = (array)$obj_2;
-//                $symbol = Common::findSymbol($obj_2->board_type_name);
-//
-//                if (isset($symbol['idx'])) {
-//                    $array[$i]['datas'][$j]['symbol'] = $symbol['symbol'];
-//                    $array[$i]['datas'][$j]['standard'] = $symbol['standard'];
-//                }
-//                $j++;
-//            }
-//
-//            $i++;
-//        }
-//
-//        $chart_arr = array();
-//
-//        foreach ($array as $k_1 => $v_1) {
-//            $chart_arr[$k_1]['tag_name'] = "myChart_".$v_1['info']['idx'];
-//            $chart_arr[$k_1]['config'] = array(
-//                'type'      => 'line',
-//                'data'      =>  array('labels'=> array(), 'datasets'=>array()),
-//                'options'    =>  array(
-//                    'plugins' => array(
-//                        'legend' => array(
-//                            'position' => 'bottom',
-//                        ),
-//                    ),
-//                ),
-//
-//            );
-//
-//            $chart_data_array = array();
-//            foreach ($v_1['datas'] as $k_2 => $v_2) {
-//                $chart_data_array['label'] = $v_2['board_type_name'];
-//                $chart_data_array['borderColor'] = "rgb(0, 0, 255)";
-//                $chart_data_array['backgroundColor'] = "rgb(0, 0, 255)";
-//                $chart_data_array['tension'] = 0.1;
-//                $chart_data_array['pointStyle'] = false;
-//                $chart_data_array['data'] = array();
-//
-//                if ($v_2['symbol'] == 'L') {
-//                    $result_3 = EntityRawData::AccumulateDatas($v_2['address'], $v_2['board_type'], $v_2['board_type_field'], $v_2['board_type_name'],'0', '10');
-//                } else {
-//                    $result_3 = EntityRawData::AvgDatas($v_2['address'], $v_2['board_type'], $v_2['board_type_field'], $v_2['board_type_name'],'0', '10');
-//                }
-//                while ($obj = $result_3->fetchObject(EntityRawData::class)) {
-////                    if ($k_2 == 0) {
-////                        array_push($chart_arr[$k_1]['config']['data']['labels'], substr( $obj->created, 11, 5) );
-////                    }
-//                    array_push($chart_data_array['data'],
-//                        array(
-//                            'y' => round($obj->{$v_2['board_type_name']},1),
-//                            'x' => $obj->created,
-//                        )
-//                    );
-//
-////                    array_push($chart_data_array['data'], round($obj->{$v_2['board_type_name']},1));
-//                }
-//
-//                array_push($chart_arr[$k_1]['config']['data']['datasets'], $chart_data_array);
-//            }
-//        }
-//
-//        return [
-//            'success' => true,
-//            'obj' => $chart_arr
-//        ];
-//    }
-
 
     public static function getWidgetItems($request) {
         $postVars = $request->getPostVars();
